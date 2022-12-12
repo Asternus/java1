f912852821d6fa7585e94f17e26cdc397cf44a37
Asternus <malafiko@gmail.com>
2022-12-12 20:39:15 +0200

    hol

--- com/src/Main.java
+++ com/src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         final Student1 student3 = new Student1();
         final Student1 student4 = new Student1();
         final Student1 student5 = new Student1();
+        Test test = new Test();
+        Test test2 = new Test();
+        Test test3 = new Test();
     }
 }
