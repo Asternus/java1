3fa55cd89e417242de20d596e24ccf9f116abd1a
Asternus <malafiko@gmail.com>
2022-12-12 17:46:05 +0200

    1

---
+++ com/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        final Student1 student1 = new Student1();
+        final Student1 student2 = new Student1();
+        final Student1 student3 = new Student1();
+    }
+}
