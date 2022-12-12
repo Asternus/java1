bd4e2868bf442b5b2f2845b60798817fef5d9899
Asternus <malafiko@gmail.com>
2022-12-12 17:53:31 +0200

    1

--- com/src/Main.java
+++ com/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         final Student1 student1 = new Student1();
         final Student1 student2 = new Student1();
         final Student1 student3 = new Student1();
+        final Student1 student4 = new Student1();
     }
 }
